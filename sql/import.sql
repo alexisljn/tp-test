6f46fced22724eab2b07dfcfa68a180546f46139
Alexis <alexis.lejeune@gmx.fr>
2020-04-13 10:54:52 +0200

    remove done

--- sql/import.sql
+++ sql/import.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : database
--- Généré le : Dim 12 avr. 2020 à 09:30
+-- Généré le : lun. 13 avr. 2020 à 08:46
 -- Version du serveur :  10.4.11-MariaDB-1:10.4.11+maria~bionic
 -- Version de PHP : 7.4.1
 
@@ -31,8 +31,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:simple_array)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `roles`) VALUES
+(2, 'admin@admin.fr', '$2y$13$ITxdD8gNfrTgRVzR97AteO94j7RW5kpaJQQj1lCK1GnoF33QcdXre', 'ROLE_ADMIN');
 
 -- --------------------------------------------------------
 
@@ -45,8 +53,18 @@ CREATE TABLE `contact` (
   `firstname` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
   `lastname` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `phone_number` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `phone_number` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `contact`
+--
+
+INSERT INTO `contact` (`id`, `firstname`, `lastname`, `email`, `phone_number`) VALUES
+(1, 'Bob', 'Johnson', 'bobbyj@usa.fr', '0662556300'),
+(3, 'Patrice', 'Laffonte', 'pat@lafonte.fr', '0101010101'),
+(4, 'Laurent', 'Gers', 'bobbyj@usa.f', '0123320110'),
+(7, 'Toto', 'Momo', 'toto@momo.fr', '0123456789');
 
 --
 -- Index pour les tables déchargées
@@ -62,7 +80,9 @@ ALTER TABLE `admin`
 -- Index pour la table `contact`
 --
 ALTER TABLE `contact`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_4C62E638E7927C74` (`email`),
+  ADD UNIQUE KEY `UNIQ_4C62E6386B01BC5B` (`phone_number`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -72,13 +92,13 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT pour la table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
